1f4e9fcb8796e931764085f129a73a3d3420f97f
Rafael Santos <rafa.05125@gmail.com>
2022-09-10 17:28:13 -0300

    Sessao de autenticacao com Token JWT

--- backend/src/modules/users/services/CreateSessionsService.ts
+++ backend/src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -9,8 +10,13 @@ interface IRequest {
     password: string;
 }
 
+interface IResponse {
+    user: User;
+    token: string;
+}
+
 class CreateSessionsService {
-    public async execute({ email, password }: IRequest): Promise<User> {
+    public async execute({ email, password }: IRequest): Promise<IResponse> {
         const usersRepository = getCustomRepository(UsersRepository);
         const user = await usersRepository.findByEmail(email);
 
@@ -23,7 +29,16 @@ class CreateSessionsService {
         if (!passwordConfirmerd) {
             throw new AppError('Incorret e-mail/password combination', 401);
         }
-        return user;
+
+        const token = sign({}, '740bed576eacfb22721609595b02d972', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token,
+        };
     }
 }
 
