54edfbd792b41ef47dddc8350874848f464440b9
Rafael Santos <rafa.05125@gmail.com>
2022-10-28 17:42:52 -0300

    Jest adicionado

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -35,8 +35,6 @@ app.use(
             });
         }
 
-        console.log(error);
-
         return response.status(500).json({
             status: 'error',
             message: 'Internal server error',
